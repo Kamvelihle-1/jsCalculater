ce1156565db17300fcc5f6af9671455fa5018fc3
SmasherK <ksikalan@gmail.com>
2023-05-22 16:27:23 +0200

    edits

--- script/code.js
+++ script/code.js
@@ -4,9 +4,19 @@ let num1=0;
 let num2=0;
 let isClicked=false;
 let dot = document.querySelector('#dot')
+let op="";
+let cl=false;
 
 function display(x){
-    output.value+=x
+   
+    if (cl) {
+        clear();
+        cl=false;
+    }else{
+        output.value+=x;
+        isButtonClicked(op,false);
+    }
+
 }
 
 dot.addEventListener('click', ()=>{
@@ -14,20 +24,20 @@ dot.addEventListener('click', ()=>{
         isButtonClicked(dot,true)
     }
 })
+
 function isButtonClicked(btn,ft) {
     isClicked=ft;
     btn.disabled=ft;
 }
 
 // numbers
-// document.querySelector('#numbers').addEventListener('click',()=>{
-    
-// })
+
 
 // Clear
 
 function clear() {
     output.value = " ";
+    isButtonClicked(dot,false);
 }
 document.querySelector('#clear').addEventListener('click',()=> {
     clear();
@@ -40,35 +50,52 @@ document.querySelector('#backspace').addEventListener('click', ()=>{
 
 // Addition
 document.querySelector('#plus').addEventListener('click', ()=>{
-    
+    op = document.querySelector('#plus');
     num1 = output.value;
     action= "+";
     output.value+=action;
     isButtonClicked(dot,false);
+    if (!isClicked) {
+        isButtonClicked(op,true)
+    }
  })
 // Minus
 document.querySelector('#minus').addEventListener('click', ()=>{
+    op=document.querySelector('#minus');
     num1 = output.value;
     action= "-";
     output.value+=action;
     isButtonClicked(dot,false);
+    if (!isClicked) {
+        isButtonClicked(op,true)
+    }
  })
 // Division
 document.querySelector('#divide').addEventListener('click', ()=>{
+    op=document.querySelector('#divide')
     num1 = output.value;
     action= "/";
     output.value+=action;
     isButtonClicked(dot,false);
+    if (!isClicked) {
+        isButtonClicked(op,true)
+    }
  })
 // Multiplication
 document.querySelector('#times').addEventListener('click', ()=>{
+    op=document.querySelector('#times')
     num1 = output.value;
     action= "*";
     output.value+=action;
     isButtonClicked(dot,false);
+   
+    if (!isClicked) {
+        isButtonClicked(op,true)
+    }
  })
 // Equal
 document.querySelector('#equal').addEventListener('click',()=>{
+    cl=true;
     for (let index = 0; index < output.value.length; index++) {
         if (output.value[index] == "/" || output.value[index] == "+" || output.value[index] == "-" || output.value[index] == "*"  ) {
              num2= output.value.slice(index+1,output.value.length);
@@ -76,6 +103,6 @@ document.querySelector('#equal').addEventListener('click',()=>{
         }
          
      }
-    output.value = eval(num1 + action + num2);
+    output.value = eval(num1 + action + num2).toFixed(2);
 })
 
